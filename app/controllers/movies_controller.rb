a6819ecb77884f46172e88adaf767dc43934a433
Grant Hoffmann <ghoffma3@uncc.edu>
2017-10-17 15:55:39 +0000

    Done with Section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,9 +4,21 @@ class MoviesController < ApplicationController
     end
     
     def show
-     id = params[:id] # retrieve movie ID from URI route
-     @movie = Movie.find(id) # look up movie by unique ID
-     # will render app/views/movies/show.html.haml by default
+        id = params[:id] # retrieve movie ID from URI route
+        @movie = Movie.find(id) # look up movie by unique ID
+        # will render app/views/movies/show.html.haml by default
+    end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        params.require(:movie)
+        permitted = params[:movie].permit(:title,:rating,:release_date)
+        @movie = Movie.create!(permitted)
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
     end
     
 private
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    def create
+        params.require(:movie)
+        params[:movie].permit(:title,:rating,:release_date)
+        # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
+        # rest of code...
+    end
 end
