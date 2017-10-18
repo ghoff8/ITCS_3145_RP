c2fcd681319b792a4bcb0474a943cc01c43578ac
Grant Hoffmann <ghoffma3@uncc.edu>
2017-10-18 01:30:57 +0000

    Done with Section 4.8

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,2 @@
 class Movie < ActiveRecord::Base
-    def create
-        params.require(:movie)
-        params[:movie].permit(:title,:rating,:release_date)
-        # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
-        # rest of code...
-    end
 end
